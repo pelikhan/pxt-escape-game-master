bbd006ae92bc9f85630df449ff2321d79b4892ce
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-12-15 22:18:17 -0800

    Update pxt.json, game.ts

--- game.ts
+++ game.ts
@@ -1,15 +1,21 @@
-basic.showString("ESCAPE")
 radio.setTransmitPower(7)
-escape.onMessageReceived((msg, data) => {
+basic.showString("ESCAPE")
+
+// disable default handler
+let start = input.runningTime()
+let totalSec = escape.TOTAL_SECONDS
+let remaining = totalSec
+
+radio.onReceivedBuffer(function () {
     led.toggle(0, 0)
 })
 input.onButtonPressed(Button.A, function () {
-    escape.broadcastMessage(escape.REMOVE_MINUTE)
+    totalSec = Math.min(0, totalSec - 60)
     basic.showIcon(IconNames.SmallHeart)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    escape.broadcastMessage(escape.ADD_MINUTE)
+    totalSec = Math.max(0, totalSec + 60)
     basic.showIcon(IconNames.Heart)
     basic.clearScreen()
 })
@@ -20,4 +26,24 @@ input.onButtonPressed(Button.AB, function () {
     }
     basic.showIcon(IconNames.Chessboard)
     basic.clearScreen()
+})
+
+
+// game loop
+basic.forever(function () {
+    const elapsed = (input.runningTime() - start) / 1000;
+    remaining = Math.max(0, totalSec - elapsed); // seconds
+
+    const b = control.createBuffer(5);
+    b[0] = escape.REMAINING_SECONDS;
+    b.setNumber(NumberFormat.Int32LE, 1, remaining | 0);
+    radio.sendBuffer(b);
+
+    if (remaining <= 0) {
+        basic.pause(10)
+        const bo = pins.createBuffer(1)
+        bo[0] = escape.TIME_OVER
+        radio.sendBuffer(bo)
+    }
+    basic.pause(500)
 })
